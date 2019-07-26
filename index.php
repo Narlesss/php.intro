67d2d093d39121483ca9c337146dab7cd99caed2
NArles Silva <narlessilva@gmail.com>
2019-07-26 19:46:41 -0300

    script inicial

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="utf-8">
+	<title>Testes </title>
+</head>
+<body>
+
+<nav class="container text-blue">
+	<li><h4>Calculadora</h4></li>
+	<li><h4>Meu Perfil</h4></li>
+</nav>
+
+
+
+echo Soma();
+</body>
+</html>
